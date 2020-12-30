272b5ebb0e8fc4a657a9fca984f793808477f1cf
Mounir Ben Kasmiya <mounir.benkasmiya@gmail.com>
2020-12-30 01:21:04 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    preserveHtmlElements: false,
+    content: ["./*.html"],
+  },
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
